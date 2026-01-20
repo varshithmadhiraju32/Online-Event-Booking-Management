f48e54389b0ac9fba653071fd4c04b3f4cc3f3fd
varshithmadhiraju32 <varshithmadhiraju4@gmail.com>
2026-01-20 22:54:24 +0530

    update all files

--- app.py
+++ app.py
@@ -513,3 +513,4 @@ def organizer_edit_event(event_id):
 # ================= RUN =================
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False, threaded=False)
+#====END=====
